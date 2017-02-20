c8df75ed6e163a70bea2f5b33a8c55f4c02954dc
James Frazier <jfrazier@psstudios.com>
2017-02-20 11:19:40 -0700

    Added favicons.

--- wp-content/themes/liberty-wildlife/header.php
+++ wp-content/themes/liberty-wildlife/header.php
@@ -16,6 +16,12 @@
 <html <?php language_attributes(); ?> class="<?php echo "html_{$style} ".$responsive." ".$preloader." ".$av_lightbox." ".$filterable_classes ?> ">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/manifest.json">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#ffffff">
 <?php
 /*
  * outputs a rel=follow or nofollow tag to circumvent google duplicate content for archives
