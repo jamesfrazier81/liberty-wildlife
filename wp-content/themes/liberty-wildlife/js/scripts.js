3b8ac6c1b0e727093a0a815c7d99c54bed8f75a2
James Frazier <jfrazier@psstudios.com>
2017-03-10 20:05:02 -0700

    Uninstalled Event plugin. Global and specific home page styles.
    Added docs and resources.

--- wp-content/themes/liberty-wildlife/js/scripts.js
+++ wp-content/themes/liberty-wildlife/js/scripts.js
@@ -1,11 +1,11 @@
 jQuery(function($) {
-	$(window).scroll(function() {    
-	    var scroll = $(window).scrollTop();
+	// $(window).scroll(function() {    
+	//     var scroll = $(window).scrollTop();
 
-	    if (scroll >= 200) {
-	        $('.av-section-bottom-logo').addClass('fixed animated slideInDown');
-	    } else {
-	        $('.av-section-bottom-logo').removeClass('fixed animated slideInDown');
-	    }
-	});
+	//     if (scroll >= 200) {
+	//         $('.av-section-bottom-logo').addClass('fixed animated slideInDown');
+	//     } else {
+	//         $('.av-section-bottom-logo').removeClass('fixed animated slideInDown');
+	//     }
+	// });
 });
